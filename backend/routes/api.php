d115bb48e4adab80b44dc88a08546585581a9733
Lawrence Mindanao <mindanaolawrence86@gmail.com>
2025-04-28 02:08:09 +0800

    api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,3 +6,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/products/search', [ProductController::class, 'search']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{product}', [ProductController::class, 'update']);
+    Route::delete('/products/{product}', [ProductController::class, 'destroy']);
+
+    Route::post('/checkout', [OrderController::class, 'store']);
+
+    Route::get('/checkouts', [CheckoutMonitorController::class, 'index']);
+    Route::get('/checkouts/{order}', [CheckoutMonitorController::class, 'show']);
+});
